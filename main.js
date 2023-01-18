b3b93ba24844a87f41e6b0210b65fb5060979c68
Shahzod12 <104401201+Shahzod12@users.noreply.github.com>
2023-01-18 17:31:56 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function createCapture()()
+{
+string "VIDEO"
+size() p5.js (x Width : y Height)
+ml5.js
+}
+function posenet() {
+    ml5.js 
+   var VIDEO 
+}
+function setup() {
+ var posenet  ml5.js on()
+ pose string gotposes()
+}
+function gotposes() {
+    if () {
+        length var results > 0 console results var
+}
